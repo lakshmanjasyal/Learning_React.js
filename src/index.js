94ac238f7e1ba98aaba6df3afb67845b0985cb4d
lakshu <148551822+lakshmanjasyal@users.noreply.github.com>
2024-11-03 05:49:42 +0000

    changes in index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./../public/styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(<App />, document.getElementById("root"));
 
 //1. Apply CSS styles to App.jsx component
 //to match the appearance on the completed app:
